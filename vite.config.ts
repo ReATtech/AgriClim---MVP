1382ea03e7b6baf3c843a141920436485b221b2a
elsonkjimmy <elsonkjimmy@gmail.com>
2025-04-10 14:31:31 +0100

    fix port problem

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  preview: {
+    allowedHosts: ['agriclim-mvp.onrender.com'],
+  },
   server: {
     port: 8080,
   },
